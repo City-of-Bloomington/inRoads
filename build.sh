894e243ed01a4e2c3ac9ca997729f1b76af84ab1
inghamn <inghamn@bloomington.in.gov>
2014-11-11 13:52:59 -0500

    Added Openlayers library to the build script The build script now downloads and installs Openlayers, since we don't want to have other developers need to manage all the dependencies for building Openlayers.

--- build.sh
+++ build.sh
@@ -12,4 +12,11 @@ fi
 
 rsync -rlv --exclude-from=./buildignore --delete ./ ./build/
 
+wget https://github.com/openlayers/ol3/releases/download/v3.0.0/v3.0.0.zip -O ./build/public/js/vendor/ol3.zip
+cd build/public/js/vendor
+unzip ol3.zip
+mv v3.0.0 ol3
+rm ol3.zip
+cd ../../../../
+
 tar czvf $DIST/Blossom.tar.gz --transform=s/build/Blossom/ $BUILD
