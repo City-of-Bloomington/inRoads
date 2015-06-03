b2ec29c4794bcf2ed3e8d3aa3b4237b92efb65aa
inghamn <inghamn@bloomington.in.gov>
2015-06-03 11:25:05 -0400

    Replaced OpenLayers with dist version There should no longer be any confusion over the URL to the OpenLayers resources

--- build.sh
+++ build.sh
@@ -1,30 +1,14 @@
 #!/bin/bash
+APPNAME=road_closings
 DIR=`pwd`
 BUILD=$DIR/build
-DIST=$DIR/dist
-
-OPENLAYERS=public/js/vendor/ol3
 
 if [ ! -d $BUILD ]
 	then mkdir $BUILD
 fi
 
-if [ ! -d $DIST ]
-	then mkdir $DIST
-fi
-
 # The PHP code does not need to actually build anything.
 # Just copy all the files into the build
-rsync -rlv --exclude-from=$DIR/buildignore --delete $DIR/ $BUILD/
-
-# Build the OpenLayers javascript library
-if [ ! -d $BUILD/$OPENLAYERS ]
-    then mkdir -p $BUILD/$OPENLAYERS
-fi
-cd $DIR/$OPENLAYERS
-./build.py build
-cd $DIR
-rsync -rlv $DIR/$OPENLAYERS/build/ $BUILD/$OPENLAYERS/build/
-
-# Create a distribution tarball of the build
-tar czvf $DIST/Blossom.tar.gz --transform=s/build/Blossom/ $BUILD
+rsync -rlv --exclude-from=$DIR/buildignore --delete $DIR/ $BUILD/$APPNAME
+cd $BUILD
+tar czvf $APPNAME.tar.gz $APPNAME
