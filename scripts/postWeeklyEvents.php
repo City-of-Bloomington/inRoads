e20d0ad989194c6b0404b871db2759eed975d97c
inghamn <inghamn@bloomington.in.gov>
2015-07-09 15:39:56 -0400

    Google group posting uses the same default event types as the search form Fixes #27

--- scripts/postWeeklyEvents.php
+++ scripts/postWeeklyEvents.php
@@ -5,6 +5,7 @@
  * @license http://www.gnu.org/licenses/agpl.txt GNU/AGPL, see LICENSE.txt
  * @author Cliff Ingham <inghamn@bloomington.in.gov>
  */
+use Application\Models\EventType;
 use Application\Models\GoogleGateway;
 use Blossom\Classes\Block;
 
@@ -12,12 +13,28 @@ use Blossom\Classes\Block;
 // road_closings installation.
 include '../configuration.inc';
 
-$start = new \DateTime();
-$end   = new \DateTIme('+1 week');
+$start = new \DateTime('+1 monday');
+$end   = new \DateTIme('+2 monday');
 
-$list = GoogleGateway::getEvents(GOOGLE_CALENDAR_ID, $start, $end);
+$filters = ['eventTypes'=>[]];
+foreach (EventType::types() as $type) {
+    if ($type->isDefaultForSearch()) {
+        $filters['eventTypes'][] = $type->getCode();
+    }
+}
+$list = GoogleGateway::getEvents(GOOGLE_CALENDAR_ID, $start, $end, $filters);
 $block = new Block('events/list.inc', ['events'=>$list]);
-$message = $block->render('txt');
-$subject = 'Road closings for the week: '.$start->format(DATETIME_FORMAT).' to '.$end->format(DATETIME_FORMAT);
+
+$group = substr(GOOGLE_GROUP, 0, strpos(GOOGLE_GROUP, '@'));
+
+$message = "Here are the upcoming road related events for next week, ";
+$message.= $start->format(DATE_FORMAT).' to '.$end->format(DATE_FORMAT).":\n\n";
+$message.= $block->render('txt');
+$message.= "
+To update your subscription status, please visit:
+https://groups.google.com/forum/#!forum/$group
+";
+
+$subject = 'Road closings for the week: '.$start->format(DATE_FORMAT).' to '.$end->format(DATE_FORMAT);
 $from    = 'From: '.ADMINISTRATOR_EMAIL;
 mail(GOOGLE_GROUP, $subject, $message, $from);
