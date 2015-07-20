4894b9046c228a7c07d4a12ffdc17af2881968a8
Dan Hiester <hiesterd@bloomington.in.gov>
2015-07-20 16:00:39 -0400

    Refactor the responsive stylesheet so that it modifies from small screens to large screens.

--- public/js/mobileFilters.js
+++ public/js/mobileFilters.js
@@ -1,33 +1,41 @@
-(function(){
+(function (){
     "use strict";
-    var appHeader      = document.querySelector("body > header > .container"),
-        mapFilterPanel = document.querySelector(".eventsMap > main > #panel-one"),
-        filterPanel    = document.getElementById("filterPanel"),
-        initMobileUi = function() {
+    var appHeader        = document.querySelector("body > header > .container"),
+        htmlMain         = document.querySelector("main"),
+        filterPanel      = document.getElementById("filterPanel"),
+        listPanel        = document.getElementById("panel-two"),
+        initMobileUi     = function () {
             var showFilterButton = document.createElement("div"),
-                hideFilterButton   = document.createElement("div");
+                hideFilterButton = document.createElement("div");
 
             showFilterButton.setAttribute("class", "showFilterButton");
             showFilterButton.setAttribute("id", "showFilterButton");
-            showFilterButton.textContent = "See more";
+            showFilterButton.textContent = "Change filters";
             showFilterButton.addEventListener("click", showFilters);
             appHeader.insertBefore(showFilterButton, appHeader.lastChild);
 
             hideFilterButton.setAttribute("class", "hideFilterButton");
             hideFilterButton.setAttribute("id", "hideFilterButton");
-            hideFilterButton.textContent = "Close menu";
+            hideFilterButton.textContent = "Close filters";
             hideFilterButton.addEventListener("click", hideFilters);
             filterPanel.insertBefore(hideFilterButton, filterPanel.firstChild);
 
-            
         },
-        showFilters              = function(e) {
+        showFilters = function (e) {
             var button = e.target;
-            mapFilterPanel.setAttribute("data-enabled", "true");
+            htmlMain.setAttribute("class", "filters-enabled midAnimation");
+
+            window.setTimeout(function () {
+                htmlMain.setAttribute("class", "filters-enabled");
+            }, 250);
         },
-        hideFilters              = function(e) {
+        hideFilters      = function (e) {
             var button = e.target;
-            mapFilterPanel.setAttribute("data-enabled", "false");
+            htmlMain.setAttribute("class", "midAnimation");
+
+            window.setTimeout(function () {
+                htmlMain.removeAttribute("class");
+            }, 250);
         };
 
     // Initialize the HTML element
