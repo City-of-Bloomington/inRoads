572a2517b6b44ebe1513d044b0c3b5947169d5a1
inghamn <inghamn@bloomington.in.gov>
2015-10-08 10:45:00 -0400

    Created a route for a TrafficCast feed Updates #82

---
+++ Controllers/TrafficcastController.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * @copyright 2015 City of Bloomington, Indiana
+ * @license http://www.gnu.org/licenses/agpl.txt GNU/AGPL, see LICENSE.txt
+ * @author Cliff Ingham <inghamn@bloomington.in.gov>
+ */
+namespace Application\Controllers;
+
+use Application\Models\EventType;
+use Application\Models\GoogleGateway;
+use Blossom\Classes\Block;
+use Blossom\Classes\Controller;
+use Blossom\Classes\Template;
+
+class TrafficcastController extends Controller
+{
+    const DATE_RANGE = '+30 days';
+
+    public function __construct(Template &$template)
+    {
+        parent::__construct($template);
+
+        $this->template->setOutputFormat('waze');
+    }
+
+    /**
+     * Creates variables from the searchForm submission
+     *
+     * @return array
+     */
+    private function getSearchParameters()
+    {
+        $start = new \DateTime();
+        $start->setTime(0,  0);
+
+        $end   = new \DateTime(self::DATE_RANGE);
+        $end->setTime(23, 59);
+
+        $filters['eventTypes'] = [];
+        foreach (EventType::types() as $type) {
+            if ($type->isDefaultForSearch()) { $filters['eventTypes'][] = $type->getCode(); }
+        }
+        return ['start'=>$start, 'end'=>$end, 'filters'=>$filters];
+    }
+
+    public function index()
+    {
+        $search = $this->getSearchParameters();
+        $events = GoogleGateway::getEvents(
+            GOOGLE_CALENDAR_ID,
+            $search['start'],
+            $search['end'],
+            $search['filters']
+        );
+
+        $this->template->setOutputFormat('waze');
+        $this->template->blocks[] = new Block('events/list.inc', ['events'=>$events]);
+    }
+}
