0922e4a59734b845e1dd67b58d67100b7125f10b
inghamn <inghamn@bloomington.in.gov>
2018-04-27 09:21:41 -0400

    Rewrote date handling
    We now use HTML5 date and time inputs, and expect browsers
    to do the right thing.  Internally, event startDates,
    startTimes, endDates, and endTimes are passed around as PHP
    DateTime objects.  No more string parsing everywhere!
    
    Fixes #113
    Fixes #112

---
+++ tests/UnitTests/bootstrap.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * @copyright 2018 City of Bloomington, Indiana
+ * @license http://www.gnu.org/licenses/agpl.txt GNU/AGPL, see LICENSE.txt
+ */
+$_SERVER['SITE_HOME'] = __DIR__;
+include realpath(__DIR__.'/../../bootstrap.inc');
