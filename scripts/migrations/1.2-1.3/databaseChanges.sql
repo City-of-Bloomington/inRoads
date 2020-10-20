528d4e2ff488754b605c0cb31651b54b2a57fc15
Cliff Ingham <inghamn@bloomington.in.gov>
2020-10-20 09:54:19 -0400

    Replaced Skidder with Graylog support
    Updates #203

--- scripts/migrations/1.2-1.3/databaseChanges.sql
+++ scripts/migrations/1.2-1.3/databaseChanges.sql
@@ -1,4 +1,4 @@
 alter table events add temp geometry SRID 4326 after geography;
 update events set temp=ST_GeomFromText(ST_AsText(geography), 4326);
 alter table events drop geography;
-alter table events rename column temp to geopgraphy;
+alter table events rename column temp to geography;
